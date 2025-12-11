67bef3f6271aa8b486fc8a186ee0fe57630f6d19
ser888gio <nica.sergiu@seznam.cz>
2025-12-11 21:38:18 +0100

    Added the match page

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Conversation from "./Conversation";
 import BioPage from "./BioPage";
+import MatchPage from "./MatchPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Conversation />} />
         <Route path="/bio" element={<BioPage />} />
+        <Route path="/match" element={<MatchPage />} />
       </Routes>
     </Router>
   );
